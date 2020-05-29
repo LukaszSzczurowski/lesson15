4be26f4b759c2b682ca9343626dbd1cd358292c7
Łukasz Szczurowski <lszczurowski0@gmail.com>
2020-05-29 14:07:43 +0200

    create file and read data

--- src/AppTest.java
+++ src/AppTest.java
@@ -1,5 +1,15 @@
+import java.io.IOException;
+import java.util.List;
+
 public class AppTest {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        try {
+            List<Product> productList = FileUtils.readDataFromFile("data.csv");
+            for (Product product : productList) {
+                System.out.println(product);
+            }
+        } catch (IOException e) {
+            System.err.println("Nie udało się wczytać pliku");
+        }
     }
 }
---
+++ src/FileUtils.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class FileUtils {
+
+    static ArrayList<Product> readDataFromFile(String fileName) throws IOException {
+        ArrayList<Product> productList = new ArrayList<>();
+        int size = countSize(fileName);
+        String[] helperTable = new String[size];
+        BufferedReader br = new BufferedReader(new FileReader(fileName));
+
+        for (int i = 0; i < helperTable.length; i++) {
+            helperTable[i] = br.readLine();
+            String[] split = helperTable[i].split(";");
+            String name = split[0];
+            BigDecimal price = new BigDecimal(split[1]);
+            BigDecimal tax = new BigDecimal(split[2]);
+            productList.add(new Product(name, price, tax));
+        }
+        br.close();
+        return productList;
+    }
+
+    private static int countSize(String fileName) throws FileNotFoundException {
+        Scanner scanner = new Scanner(new File(fileName));
+        int lines = 0;
+        while (scanner.hasNextLine()){
+            lines++;
+            scanner.nextLine();
+        }
+        scanner.close();
+        return lines;
+    }
+}
